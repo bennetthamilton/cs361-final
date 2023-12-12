4cb41a75f8ea5c217de26f31a11a87232865ec2c
bennetthamilton <bennetthamilton0@gmail.com>
2023-12-12 13:43:52 -0800

    fixed formatting consistencies in main, clarified variable names in main

--- gis.rb
+++ gis.rb
@@ -132,28 +132,33 @@ end
 
 
 def main()
-  w = Waypoint.new(-121.5, 45.5, 30, "home", "flag")
-  w2 = Waypoint.new(-121.5, 45.6, nil, "store", "dot")
-  ts1 = [
+
+  waypoint_1 = Waypoint.new(-121.5, 45.5, 30, "home", "flag")
+  waypoint_2 = Waypoint.new(-121.5, 45.6, nil, "store", "dot")
+
+  track_segment_1 = [
     Point.new(-122, 45),
     Point.new(-122, 46),
     Point.new(-121, 46),
   ]
 
-  ts2 = [ Point.new(-121, 45), Point.new(-121, 46), ]
+  track_segment_2 = [ 
+    Point.new(-121, 45), 
+    Point.new(-121, 46), 
+  ]
 
-  ts3 = [
+  track_segment_3 = [
     Point.new(-121, 45.5),
     Point.new(-122, 45.5),
   ]
 
-  t = Track.new([ts1, ts2], "track 1")
-  t2 = Track.new([ts3], "track 2")
+  track_1 = Track.new([track_segment_1, track_segment_2], "track 1")
+  track_2 = Track.new([track_segment_3], "track 2")
 
   null_track = NullTrack.new
   null_waypoint = NullWaypoint.new
 
-  world = World.new("My Data", [w, w2, t, t2, null_track, null_waypoint])
+  world = World.new("My Data", [waypoint_1, waypoint_2, track_1, track_2, null_track, null_waypoint])
 
   puts world.to_geojson()
 end
